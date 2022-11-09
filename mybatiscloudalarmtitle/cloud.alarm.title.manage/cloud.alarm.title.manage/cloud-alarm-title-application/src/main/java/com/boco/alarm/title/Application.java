44c61c17bdc09d900448b2440746af2ed7e17ef0
郝子天 <haozitian@boco.com.cn>
2022-11-09 18:26:40 +0800

    mybatisbanben

---
+++ mybatiscloudalarmtitle/cloud.alarm.title.manage/cloud.alarm.title.manage/cloud-alarm-title-application/src/main/java/com/boco/alarm/title/Application.java
@@ -0,0 +1,15 @@
+package com.boco.alarm.title;
+
+/**
+ * @author hao 2022/11/9 18:11
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
